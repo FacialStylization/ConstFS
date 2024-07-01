15938380d10b9b3fe5164b93fc48452b82f4d8d5
neverbiasu <1751162157@qq.com>
2024-07-01 15:18:12 +0800

    init

---
+++ app.py
@@ -0,0 +1,229 @@
+import sys
+sys.path.append('./')
+
+import os 
+import cv2
+import torch
+import random
+import numpy as np
+from PIL import Image
+from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline
+
+# import spaces
+import gradio as gr
+
+from pipeline import IPAdapterXL
+
+import os
+# os.system("git lfs install")
+# os.system("git clone https://huggingface.co/h94/IP-Adapter")
+# os.system("mv IP-Adapter/sdxl_models sdxl_models")
+
+# global variable
+MAX_SEED = np.iinfo(np.int32).max
+device = "cuda" if torch.cuda.is_available() else "cpu"
+dtype = torch.float16 if str(device).__contains__("cuda") else torch.float32
+
+# initialization
+base_model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+image_encoder_path = "IP-Adapter/sdxl_models/image_encoder"
+ip_ckpt = "IP-Adapter/sdxl_models/ip-adapter_sdxl.bin"
+
+controlnet_path = "diffusers/controlnet-canny-sdxl-1.0"
+controlnet = ControlNetModel.from_pretrained(controlnet_path, use_safetensors=False, torch_dtype=torch.float16).to(device)
+
+# load SDXL pipeline
+pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    base_model_path,
+    controlnet=controlnet,
+    torch_dtype=torch.float16,
+    add_watermarker=False,
+)
+pipe.enable_vae_tiling()
+
+# load ip-adapter
+# target_blocks=["block"] for original IP-Adapter
+# target_blocks=["up_blocks.0.attentions.1"] for style blocks only
+# target_blocks = ["up_blocks.0.attentions.1", "down_blocks.2.attentions.1"] # for style+layout blocks
+ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=["up_blocks.0.attentions.1"])
+
+def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    return seed
+
+def resize_img(
+    input_image,
+    max_side=1280,
+    min_side=1024,
+    size=None,
+    pad_to_max_side=False,
+    mode=Image.BILINEAR,
+    base_pixel_number=64,
+):
+    w, h = input_image.size
+    if size is not None:
+        w_resize_new, h_resize_new = size
+    else:
+        ratio = min_side / min(h, w)
+        w, h = round(ratio * w), round(ratio * h)
+        ratio = max_side / max(h, w)
+        input_image = input_image.resize([round(ratio * w), round(ratio * h)], mode)
+        w_resize_new = (round(ratio * w) // base_pixel_number) * base_pixel_number
+        h_resize_new = (round(ratio * h) // base_pixel_number) * base_pixel_number
+    input_image = input_image.resize([w_resize_new, h_resize_new], mode)
+
+    if pad_to_max_side:
+        res = np.ones([max_side, max_side, 3], dtype=np.uint8) * 255
+        offset_x = (max_side - w_resize_new) // 2
+        offset_y = (max_side - h_resize_new) // 2
+        res[
+            offset_y : offset_y + h_resize_new, offset_x : offset_x + w_resize_new
+        ] = np.array(input_image)
+        input_image = Image.fromarray(res)
+    return input_image
+
+# @spaces.GPU(enable_queue=True)
+def create_image(image_pil,
+                 input_image,
+                 prompt,
+                 n_prompt,
+                 scale, 
+                 control_scale, 
+                 guidance_scale,
+                 num_samples,
+                 num_inference_steps,
+                 seed,
+                 target="Load only style blocks",
+                 neg_content_prompt=None,
+                 neg_content_scale=0):
+
+    if target =="Load original IP-Adapter":
+        # target_blocks=["blocks"] for original IP-Adapter
+        ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=["blocks"])
+    elif target=="Load only style blocks":
+        # target_blocks=["up_blocks.0.attentions.1"] for style blocks only
+        ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=["up_blocks.0.attentions.1"])
+    elif target == "Load style+layout block":
+        # target_blocks = ["up_blocks.0.attentions.1", "down_blocks.2.attentions.1"] # for style+layout blocks
+        ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=["up_blocks.0.attentions.1", "down_blocks.2.attentions.1"])
+    
+    if input_image is not None:
+        input_image = resize_img(input_image, max_side=1024)
+        cv_input_image = pil_to_cv2(input_image)
+        detected_map = cv2.Canny(cv_input_image, 50, 200)
+        canny_map = Image.fromarray(cv2.cvtColor(detected_map, cv2.COLOR_BGR2RGB))
+    else:
+        canny_map = Image.new('RGB', (1024, 1024), color=(255, 255, 255))
+        control_scale = 0
+
+    if float(control_scale) == 0:
+        canny_map = canny_map.resize((1024,1024))
+    
+    if len(neg_content_prompt) > 0 and neg_content_scale != 0:
+        images = ip_model.generate(pil_image=image_pil,
+                                prompt=prompt,
+                                negative_prompt=n_prompt,
+                                scale=scale,
+                                guidance_scale=guidance_scale,
+                                num_samples=num_samples,
+                                num_inference_steps=num_inference_steps, 
+                                seed=seed,
+                                pulid_image=cv_input_image,
+                                image=canny_map,
+                                controlnet_conditioning_scale=float(control_scale),
+                                neg_content_prompt=neg_content_prompt,
+                                neg_content_scale=neg_content_scale
+                                )
+    else:
+        images = ip_model.generate(pil_image=image_pil,
+                                prompt=prompt,
+                                negative_prompt=n_prompt,
+                                scale=scale,
+                                guidance_scale=guidance_scale,
+                                num_samples=num_samples,
+                                num_inference_steps=num_inference_steps, 
+                                seed=seed,
+                                pulid_image=cv_input_image,
+                                image=canny_map,
+                                controlnet_conditioning_scale=float(control_scale),
+                                )
+    return images
+
+def pil_to_cv2(image_pil):
+    image_np = np.array(image_pil)
+    image_cv2 = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+    return image_cv2
+
+# Description
+title = r"""
+<h1 align="center">Instant Facial Stylization Experiment</h1>
+"""
+
+
+block = gr.Blocks(css="footer {visibility: hidden}").queue(max_size=10, api_open=False)
+with block:
+    
+    # description
+    gr.Markdown(title)
+    
+    with gr.Tabs():
+        with gr.Row():
+            with gr.Column():
+                with gr.Column():
+                    src_image_pil = gr.Image(label="Source Image (optional)", type='pil')
+                    control_scale = gr.Slider(minimum=0,maximum=1.0, step=0.01,value=0.5, label="Controlnet conditioning scale")
+                    
+                    n_prompt = gr.Textbox(label="Neg Prompt", value="text, watermark, lowres, low quality, worst quality, deformed, glitch, low contrast, noisy, saturation, blurry")
+                    
+                    neg_content_prompt = gr.Textbox(label="Neg Content Prompt", value="")
+                    neg_content_scale = gr.Slider(minimum=0, maximum=1.0, step=0.01,value=0.5, label="Neg Content Scale")
+
+                    guidance_scale = gr.Slider(minimum=1,maximum=15.0, step=0.01,value=5.0, label="guidance scale")
+                    num_samples= gr.Slider(minimum=1,maximum=4.0, step=1.0,value=1.0, label="num samples")
+                    num_inference_steps = gr.Slider(minimum=5,maximum=50.0, step=1.0,value=20, label="num inference steps")
+                    seed = gr.Slider(minimum=-1000000,maximum=1000000,value=1, step=1, label="Seed Value")
+                    randomize_seed = gr.Checkbox(label="Randomize seed", value=True)
+            with gr.Column():
+                
+                with gr.Row():
+                    with gr.Column():
+                        image_pil = gr.Image(label="Style Image", type='pil')
+                
+                target = gr.Radio(["Load only style blocks", "Load style+layout block", "Load original IP-Adapter"], 
+                                  value="Load style+layout block",
+                                  label="Style mode")
+                
+                prompt = gr.Textbox(label="Prompt",
+                                    value="masterpiece, best quality, high quality")
+                
+                scale = gr.Slider(minimum=0,maximum=2.0, step=0.01,value=1.0, label="Scale")
+                
+                generate_button = gr.Button("Generate Image")
+                
+                generated_image = gr.Gallery(label="Generated Image")
+
+        generate_button.click(
+            fn=randomize_seed_fn,
+            inputs=[seed, randomize_seed],
+            outputs=seed,
+            queue=False,
+            api_name=False,
+        ).then(
+            fn=create_image,
+            inputs=[image_pil,
+                    src_image_pil,
+                    prompt,
+                    n_prompt,
+                    scale, 
+                    control_scale, 
+                    guidance_scale,
+                    num_samples,
+                    num_inference_steps,
+                    seed,
+                    target,
+                    neg_content_prompt,
+                    neg_content_scale], 
+            outputs=[generated_image])
+
+block.launch()
---
+++ pipeline.py
@@ -0,0 +1,113 @@
+from pulid.pipeline import PuLIDPipeline
+from ip_adapter.pipeline import IPAdapter
+from PIL import Image
+from typing import List
+import torch
+
+pipeline = PuLIDPipeline()
+
+def get_generator(seed, device):
+
+    if seed is not None:
+        if isinstance(seed, list):
+            generator = [torch.Generator(device).manual_seed(seed_item) for seed_item in seed]
+        else:
+            generator = torch.Generator(device).manual_seed(seed)
+    else:
+        generator = None
+
+    return generator
+
+class IPAdapterXL(IPAdapter):
+    """SDXL"""
+
+    def generate(
+        self,
+        pil_image,
+        prompt=None,
+        negative_prompt=None,
+        scale=1.0,
+        num_samples=4,
+        seed=None,
+        num_inference_steps=30,
+        neg_content_emb=None,
+        neg_content_prompt=None,
+        neg_content_scale=1.0,
+        **kwargs,
+    ):  
+        # print('kwargs', kwargs)
+        image = kwargs['pulid_image']
+        kwargs.pop('pulid_image')
+        id_embedding = pipeline.get_id_embedding(image)
+        
+        self.set_scale(scale)
+
+        num_prompts = 1 if isinstance(pil_image, Image.Image) else len(pil_image)
+
+        if prompt is None:
+            prompt = "best quality, high quality"
+        if negative_prompt is None:
+            negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
+
+        if not isinstance(prompt, List):
+            prompt = [prompt] * num_prompts
+        if not isinstance(negative_prompt, List):
+            negative_prompt = [negative_prompt] * num_prompts
+        
+        if neg_content_emb is None:
+            if neg_content_prompt is not None:
+                with torch.inference_mode():
+                    (
+                        prompt_embeds_, # torch.Size([1, 77, 2048])
+                        negative_prompt_embeds_,
+                        pooled_prompt_embeds_, # torch.Size([1, 1280])
+                        negative_pooled_prompt_embeds_,
+                    ) = self.pipe.encode_prompt(
+                        neg_content_prompt,
+                        num_images_per_prompt=num_samples,
+                        do_classifier_free_guidance=True,
+                        negative_prompt=negative_prompt,
+                    )
+                    pooled_prompt_embeds_ *= neg_content_scale
+            else:
+                pooled_prompt_embeds_ = neg_content_emb
+        else:
+            pooled_prompt_embeds_ = None
+
+        image_prompt_embeds, uncond_image_prompt_embeds = self.get_image_embeds(pil_image, content_prompt_embeds=pooled_prompt_embeds_)
+        # print('pooled_prompt_embeds_', pooled_prompt_embeds_)
+        bs_embed, seq_len, _ = image_prompt_embeds.shape
+        image_prompt_embeds = image_prompt_embeds.repeat(1, num_samples, 1)
+        image_prompt_embeds = image_prompt_embeds.view(bs_embed * num_samples, seq_len, -1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.repeat(1, num_samples, 1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.view(bs_embed * num_samples, seq_len, -1)
+
+        with torch.inference_mode():
+            (
+                prompt_embeds,
+                negative_prompt_embeds,
+                pooled_prompt_embeds,
+                negative_pooled_prompt_embeds,
+            ) = self.pipe.encode_prompt(
+                prompt,
+                num_images_per_prompt=num_samples,
+                do_classifier_free_guidance=True,
+                negative_prompt=negative_prompt,
+            )
+            prompt_embeds = torch.cat([prompt_embeds, image_prompt_embeds], dim=1)
+            negative_prompt_embeds = torch.cat([negative_prompt_embeds, uncond_image_prompt_embeds], dim=1)
+
+        self.generator = get_generator(seed, self.device)
+        
+        images = self.pipe(
+            prompt_embeds=prompt_embeds,
+            negative_prompt_embeds=negative_prompt_embeds,
+            pooled_prompt_embeds=pooled_prompt_embeds,
+            negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+            num_inference_steps=num_inference_steps,
+            generator=self.generator,
+            id_embedding=id_embedding,
+            **kwargs,
+        ).images
+    
+        return images
